e940ee9e5fa4213a3061d1df99b4e13bc5a6c386
GSphd312 <hper.h263@gmail.com>
2026-09-18 14:47:39 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,9 +2,11 @@
 // For example: window.IMRAD_FIREBASE_CONFIG = { apiKey: "...", authDomain: "...", projectId: "...", appId: "..." };
 // The web configuration is public. NEVER paste a service-account or admin private key.
 window.IMRAD_FIREBASE_CONFIG = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyA0QkUO9UhFdq450-iTw1Lh0UI--ws-6Jk",
+  authDomain: "imrad-writing-lab.firebaseapp.com",
+  projectId: "imrad-writing-lab",
+  storageBucket: "imrad-writing-lab.firebasestorage.app",
+  messagingSenderId: "310206880538",
+  appId: "1:310206880538:web:b4371a67888103d0b9eaa3",
+  measurementId: "G-EGXTBS1XR6"
 };
